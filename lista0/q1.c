12542074f669b10d0e64282c4d3c4b2f90d03ff9
Júlia <juliacoite@discente.ufg.br>
2025-08-22 17:25:18 -0300

    fazendo a questao 2

--- lista0/q1.c
+++ lista0/q1.c
@@ -3,9 +3,9 @@
 int main() {
     float salariomin, quantkw;
 
-    printf("Insira o valor do salario minimo: ");
+    // printf("Insira o valor do salario minimo: ");
     scanf("%f", &salariomin);
-    printf("\nInsira a quantidade de kW gasta pela residencia: ");
+    // printf("\nInsira a quantidade de kW gasta pela residencia: ");
     scanf("%f", &quantkw);
 
     float parteDoSalario = salariomin*0.7;
@@ -15,9 +15,9 @@ int main() {
     float valorPorKw = valorASerPago/quantkw;
     float valorComDesconto = valorASerPago*0.9;
 
-    printf("\nCusto por kW: R$ %f\n", valorPorKw);
-    printf("\nCusto do consumo: R$ %f\n", valorASerPago);
-    printf("\nCusto com desconto: R$ %f\n", valorComDesconto);
+    printf("\nCusto por kW: R$ %.2lf\n", valorPorKw);
+    printf("\nCusto do consumo: R$ %.2lf\n", valorASerPago);
+    printf("\nCusto com desconto: R$ %.2lf\n", valorComDesconto);
 
     return 0;   
 }
---
+++ lista0/q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+float fahrenheit;
+// usuario insere a temperatura em F
+scanf("%f", &fahrenheit);
+float polegadas;
+// chuva em polegadas
+scanf("%f", &polegadas);
+
+float celsius = (5*(fahrenheit - 32))/9;
+float chuvaEmMili = polegadas*25.4;
+
+printf("O VALOR EM CELSIUS = ")
+}
