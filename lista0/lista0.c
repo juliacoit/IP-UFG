0056b66e3af75ba46ad36e647bb9b5c671f64242
Júlia <juliacoite@discente.ufg.br>
2025-08-22 17:01:37 -0300

    questao 1 correta

--- lista0/lista0.c
+++ lista0/lista0.c
@@ -3,8 +3,10 @@
 int main() {
     float salariomin, quantkw;
 
-    scanf("Insira o valor do salario minimo: %f\n", &salariomin);
-    scanf("Insira a quantidade de kW gasta pela residencia: %f\n", &quantkw);
+    printf("Insira o valor do salario minimo: ");
+    scanf("%f", &salariomin);
+    printf("\nInsira a quantidade de kW gasta pela residencia: ");
+    scanf("%f", &quantkw);
 
     float parteDoSalario = salariomin*0.7;
     float quant100kw = quantkw/100;
@@ -13,8 +15,9 @@ int main() {
     float valorPorKw = valorASerPago/quantkw;
     float valorComDesconto = valorASerPago*0.9;
 
-    printf("Custo por kW: R$ %f\n", valorPorKw);
-    printf("Custo do consumo: R$ %f\n", valorASerPago);
-    printf("Custo com desconto: R$ %f\n", valorComDesconto);
-    
+    printf("\nCusto por kW: R$ %f\n", valorPorKw);
+    printf("\nCusto do consumo: R$ %f\n", valorASerPago);
+    printf("\nCusto com desconto: R$ %f\n", valorComDesconto);
+
+    return 0;   
 }
