62cd43d7ab530ca7e84600a3093a0edc26d02006
Júlia <juliacoite@discente.ufg.br>
2025-08-22 15:19:25 -0300

    tipos de variáveis e constantes

--- p1.c
+++ p1.c
@@ -5,7 +5,23 @@
 // iniciando o programa principal
 int main(){
 
-    printf("oi\n");
+    char ch;
+    ch = 'a'; // equivalente à ch recebe o número 97 da tabela ASCII
+    printf("%c\n", ch); // prints the character stored in ch
+
+    ch = 97; // também pode ser feito
+    printf("%c\n", ch); // prints the character stored in ch
+
+    int M;
+    // uma variável que permite armazenar constantes inteiras
+    M = 20;
+    printf("%d\n", M);
+    M = 'a';
+    printf("%d\n", M);
+    /*
+    dentro da memória RAM , um int ocupa 4 bytes
+    ou seja 32 bits, 2^32
+    */
 
     return 0;
 }
