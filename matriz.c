15193ad6f291f2e867de7f0246b742254a332964
Júlia Coité <119825956+juliacoit@users.noreply.github.com>
2025-10-14 15:37:37 -0300

    Update matriz.c
    adapting functions

--- matriz.c
+++ matriz.c
@@ -2,16 +2,33 @@
 #define NL 3
 #define NC 2
 
+void read_matrix(int A[][NC], int nl);
+void print_matrix(int A[][NC], int nl);
+
 int main(){
     int A[NL][NC];
+    
+    read_matrix(A,NL);
+    print_matrix(A, NL);
+
+    return 0;
+    
+}
+
+void read_matrix(int A[][NC], int nl){
     int l, c;
 
     for (l=0; l<NL; l++){
-        printf("Lendo a linha %d\n", l);
+        printf("Lendo a linha %d: ", l);
         for(c=0; c<NC; c++){
             scanf("%d", &A[l][c]);
         }
+        printf("\n");
     }
+}
+void print_matrix(int A[][NC], int nl){
+    int l, c;
+
     for (l=0; l<NL; l++){
         for(c=0; c<NC; c++){
             printf("%d ", A[l][c]);
