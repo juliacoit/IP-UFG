615c8a6b411e0ab7f7ad4c9b3e8628b5f2f58e02
Julha <juliascoite@gmail.com>
2025-10-22 12:58:12 -0300

    terminando a questao 4

--- matrizeslista/q4.c
+++ matrizeslista/q4.c
@@ -2,16 +2,21 @@
 
 int main(){
 
-int matriz[2][2];
+float matriz[2][2];
 float matrizQuad[2][2];
 
-scanf("%d%d%d%d", &matriz[0][0], &matriz[0][1], &matriz[1][0], &matriz[1][1]);
+scanf("%f%f%f%f", &matriz[0][0], &matriz[0][1], &matriz[1][0], &matriz[1][1]);
 
-matrizQuad[0][0] = matriz[0][0]*matriz[0][0] + matriz[0][1]*matriz[1][0];
-matrizQuad[0][1] = matriz[0][1]*matriz[0][1];
-matrizQuad[1][0] = matriz[1][0]*matriz[1][0];
-matrizQuad[1][1] = matriz[1][1]*matriz[1][1];
-// poderia ter feito um loop for mas como eh uma matriz pequena fiz na mao mesmo
+float a = matriz[0][0];
+float b = matriz[0][1];
+float c = matriz[1][0];
+float d = matriz[1][1];
+
+matrizQuad[0][0] = (a*a)+(b*c);
+matrizQuad[0][1] = (a*b)+(b*d);
+matrizQuad[1][0] = (c*a)+(d*c);
+matrizQuad[1][1] = (c*b)+(d*d);
+
 printf("%.3f %.3f\n%.3f %.3f", matrizQuad[0][0], matrizQuad[0][1], matrizQuad[1][0], matrizQuad[1][1]);
 return 0;
 }
