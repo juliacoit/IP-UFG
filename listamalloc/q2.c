eba8646d5cd7c7bc8f466284498413efe7e0f612
Julha <juliascoite@gmail.com>
2025-11-11 15:17:24 -0300

    q2 malloc

--- listamalloc/q2.c
+++ listamalloc/q2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define P 50
 
 float idaMercado (int quantDisp);
@@ -13,8 +15,8 @@ int main () {
         int quantDisp;
         scanf("%d", &quantDisp);
 
-        int totalPay = idaMercado(quantDisp);
-        printf("R$ %.2f", totalPay);
+        float totalPay = idaMercado(quantDisp);
+        printf("R$ %.2f\n", totalPay);
         x++;
     }
     
@@ -23,7 +25,7 @@ int main () {
 
 float idaMercado (int quantDisp){
     int i, j;
-    char ** characName = (char **)malloc(quantDisp * sizeof(char));
+    char ** characName = (char **)malloc(quantDisp * sizeof(char*));
     for (i = 0; i < quantDisp; i++){
         characName[i] = (char *)malloc(sizeof(char)*P);
     }
@@ -31,32 +33,32 @@ float idaMercado (int quantDisp){
     float * itemPreco = malloc(quantDisp * sizeof(float));
 
     for (i= 0; i<quantDisp; i++){
-        scanf("%c", &characName[i]);
+        scanf("%s", characName[i]);
         scanf("%f", &itemPreco[i]);
     }
 
     int buyQuant;
     scanf("%d", &buyQuant);
 
-    char ** buyName = (char **)malloc(buyQuant * sizeof(char));
+    char ** buyName = (char **)malloc(buyQuant * sizeof(char*));
 
     for (i = 0; i < buyQuant; i++){
         buyName[i] = (char *)malloc(sizeof(char)*P);
     }
 
-    int * itemQuant = malloc(buyQuant * sizeof(float));
+    int * itemQuant = malloc(buyQuant * sizeof(int));
 
     for (i= 0; i<buyQuant; i++){
-        scanf("%c", &buyName[i]);
-        scanf("%f", &itemQuant[i]);
+        scanf("%s", buyName[i]);
+        scanf("%d", &itemQuant[i]);
     }
 
     float totalPay = 0;
     // agora vou fazer um for dentro de for onde o i representa a lista principal, e j a de compras
     for (i = 0; i < quantDisp; i++){
         for (j = 0; j < buyQuant; j++)
-        if (characName[i] == buyName[j]){
-            totalPay = itemPreco[j]*itemQuant[j];
+        if (strcmp(characName[i], buyName[j]) == 0){
+            totalPay += itemPreco[i]*itemQuant[j];
         }
     }
     for (i = 0; i < quantDisp; i++){
