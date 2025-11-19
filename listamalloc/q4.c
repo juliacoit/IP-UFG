b84333b3958e8892ed80f14cc375c5b8c2344871
Júlia <juliascoite@gmail.com>
2025-11-19 02:31:04 -0300

    .

--- listamalloc/q4.c
+++ listamalloc/q4.c
@@ -1,3 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
 typedef struct vetor4d {
-    
+    float u;
+    float x;
+    float y;
+    float z;
+    float norma;
+} v4d;
+
+float normaCalculator(v4d A);
+float ** orderVector(int loop);
+
+int main(){
+
+    int loop, i = 0;
+    scanf("%d", &loop);
+
+    while (i < loop) {
+
+        float ** vetores = orderVector(loop);
+        v4d vetor4d;
+        scanf("%f%f%f%f", &vetor4d.u, &vetor4d.x, &vetor4d.y, &vetor4d.z);
+        vetor4d.norma = normaCalculator(vetor4d);
+
+        printf("Vetor: (%.2f, %.2f, %.2f, %.2f) Norma: %.2f\n", vetor4d.u, vetor4d.x, vetor4d.y, vetor4d.z, norma);
+        i++;
+    }
+
+    return 0;
+}
+
+float normaCalculator(v4d A){
+    float A.norma = sqrt(pow(A.u, 2) + pow(A.x, 2) + pow(A.y, 2) + pow(A.z, 2));
+    return A.norma;
+}
+
+float ** orderVector(int loop){
+    float ** vectors = (float **)malloc(loop * sizeof(float *));
+    int i;
+    for (i = 0; i < 5; i++){
+    vectors[i] = (float *)malloc(sizeof(float));
+    }
+
+    return vectors;
 }
